0e74f1e9a43657967931df5d26bd79feeb3ec603
Sengsathit <sengsathit@gmail.com>
2023-12-21 11:59:16 +0100

    proposition exo

--- exercice_impaire_2.py
+++ exercice_impaire_2.py
@@ -1,3 +1,4 @@
-a = %2 
-if a != 0:
-    print (list(range (10,30,)))
+def get_odds(a,b):
+    return [odd_value for odd_value in range(int(a), int(b)) if odd_value % 2 != 0]
+
+print(get_odds(2.3, 14.5))
